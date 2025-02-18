de9bd5ff67e75089c3d08e685839700759ee7af9
SyntaXPixel <SyntaXPixel@proton.me>
2025-02-18 12:11:37 +0530

    n

---
+++ ex3.py
@@ -0,0 +1,6 @@
+#Taking user input, performing a calculation, and printing the result
+
+radius = int(input("Enter Value of Radius of Your Circle : "))
+pi = 3.14
+area = pi * ((radius) ** 2)
+print(f"Area of Your Circle is : {area}")
