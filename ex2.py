d56e1fe61144620919cacd2406b34580ff710508
SyntaXPixel <SyntaXPixel@proton.me>
2025-02-18 12:04:30 +0530

    n

--- ex2.py
+++ ex2.py
@@ -1,9 +1,18 @@
-a=int(input("Enter value of A"))
-b=int(input("Enter value of B"))
+a=int(input("Enter value of A : "))
+b=int(input("Enter value of B : "))
 
 print(a+b)
 print(a/b)
 print(a-b)
 print(a*b)
 
+first_name = input("Enter First Name : ")
+last_name = input("Enter Last Name : ")
+Full_name = first_name + " " + last_name
+print(f"Your Full Name is {Full_name}")
 
+
+#True means 1 so it will be added to a (ex. a+1)
+flag = True
+c=a+flag
+print("result of boolean addition : ",c)
