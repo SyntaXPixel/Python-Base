29e58697de24e705c7a5d0233adbcf12ca92f43b
SyntaXPixel <SyntaXPixel@proton.me>
2025-02-27 13:25:22 +0530

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -16,3 +16,14 @@ print(f"Your Full Name is {Full_name}")
 flag = True
 c=a+flag
 print("result of boolean addition : ",c)
+
+#area of triangle 
+base = int(input("Enter base of triangle : "))
+height = int(input("Enter height of triangle : "))
+area = 0.5 * base * height
+print("Area of triangle is : ",area)
+
+#square root
+c = int(input("Enter Value of C : "))
+square_root = (c ** 0.5)
+print("Square Root of C is : ",square_root)
