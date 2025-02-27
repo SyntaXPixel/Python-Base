531e3e893b9786f4a2b8e2c5e005ca4ef37c2876
SyntaXPixel <SyntaXPixel@proton.me>
2025-02-27 13:25:00 +0530

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,8 @@
+# simple interest
+principle = int(input("Enter value of your principle: "))
+rate = int(input("Enter value of your rate: "))
+time = int(input("Enter value of your time: "))
+
+# calculate simple interest
+simple_interest = (principle * rate * time) / 100
+print("Your Simple Interest is: ", simple_interest)
