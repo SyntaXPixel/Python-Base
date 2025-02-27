5cad41e72671327af4571340673490d60d2a5900
SyntaXPixel <SyntaXPixel@proton.me>
2025-02-27 14:03:53 +0530

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,9 +1,9 @@
 num = int(input("Enter your number : "))
-if num == 0:
-    print("Your Number is Zero")
-elif num > 0:
+
+if num > 0:
     print("Your Number is Positive")
 elif num < 0:
     print("Your Number is Negetive")
+else:
+    print("Your Number is Zero")
     
-
