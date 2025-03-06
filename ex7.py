890ec7f403c5b200b7b7c98f7ee7b5f1e9f69fd0
SyntaXPixel <SyntaXPixel@proton.me>
2025-03-06 15:13:01 +0530

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,11 +1,29 @@
-list = input("Enter your Numbers : ")
-list = list.split(" ")
-num_list = [int(n) for n in list]
+input_list = input("Enter your Numbers : ")
+input_list = input_list.split(" ")
+num_list = [int(n) for n in input_list]
 length = len(num_list)
 
-def add(num_list, length):
-    if length < 0:
+
+def add(num_list_x, length_x): 
+
+    if length_x == 0:
      return 0
-    return num_list[length - 1] + add(num_list, length-1)
+    return num_list_x[length_x-1] + add(num_list_x, (length_x-1))
 
 print(f"{add(num_list, length)}")
+
+
+# input 50 50 50 
+# num_list = [50, 50, 50]
+# length = 3
+# def add([50, 50, 50],3):
+# [50, 50, 50] = num_list_x
+# length_x = 3
+# return num_list_x[length_x-1] + add(num_list_x, (length_x-1))
+# return ([50, 50, 50])[2] + add([50, 50, 50],2)
+
+# [50, 50, 50] = num_list_x
+# length_x = 1
+# if length_x == 0:
+# return num_list_x[length_x-1] + add(num_list_x, (length_x-1))
+# return ([50, 50, 50])[0] + add([50, 50, 50],0)
