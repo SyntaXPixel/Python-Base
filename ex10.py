87cebce9fb27018d5fc924d66d85881e3e553dfe
SyntaXPixel <SyntaXPixel@proton.me>
2025-03-06 15:12:26 +0530

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,7 +1,7 @@
-#Taking user input, performing a calculation and printing the result
 
+import math
 radius = int(input("Enter Value of Radius of Your Circle : "))
-pi = 3.14
-area = pi * ((radius) ** 2)
-print(f"Area of Your Circle is : {area}")
+area = math.pi * ((radius) ** 2)
+print(f"Area of Your Circle is : {round(area, 2)}")
 
+# round used to round off the decimal value.  
