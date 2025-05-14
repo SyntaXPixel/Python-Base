7eaab065c7daf8ac19937ed8b1ab6ced46c66693
SyntaXPixel <SyntaXPixel@proton.me>
2025-05-14 14:48:06 +0530

    Update sort list.py

--- sort list.py
+++ sort list.py
@@ -1,6 +1,5 @@
 input_list = input("Enter your Numbers : ")
 input_list = input_list.split(" ")
-num_list = [int(n) for n in input_list]
-
+num_list = list(map(int,input_list))
 num_list.sort()
 print(num_list)
